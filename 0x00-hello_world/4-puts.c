fb8cab98190ea613e69f92d6ce1a19fbb267dc4a
Sylvain Kadjo <sylvainka12@gmail.com>
2022-06-02 10:46:44 -0700

    Fix programme issue

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ *
+ * Return:  0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
