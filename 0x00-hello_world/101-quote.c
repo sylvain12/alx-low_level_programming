06dc531221912b40e5d01696fd040f089058145d
Sylvain Kadjo <sylvainka12@gmail.com>
2022-06-02 12:13:28 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <sdtio.h>
+
+/**
+* main - Prints quote message
+*
+* Return: always 1
+*/
+int main(void)
+{
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	print("%s\n", message);
+	return (1);
+}
+
