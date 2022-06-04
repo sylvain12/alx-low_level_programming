9d1eeda5bbb9a54c98aca1491194f716c0878e49
Sylvain Kadjo <sylvainka12@gmail.com>
2022-06-03 19:34:06 -0700

    Fix task 1 script

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
-		printf("%s is %d and is greater than 5\n", msg, n);
+		printf("%s %d is %d and is greater than 5\n", msg, n, n % 10);
 	else if (n == 0)
-		printf("%s is %d and is 0\n", msg, n);
+		printf("%s %d is %d and is 0\n", msg, n, n % 10);
 	else if (n < 6 && n != 0)
-		printf("%s is %d and is less than 6 and not 0\n", msg, n);
+		printf("%s %d is %d and is less than 6 and not 0\n", msg, n, n % 10);
 	return (0);
 }
