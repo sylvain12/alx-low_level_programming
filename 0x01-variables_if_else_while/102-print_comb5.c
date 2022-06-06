8a4d0c38e458eb8932ed0d5183d3ada828aeec1f
Sylvain Kadjo <sylvainka12@gmail.com>
2022-06-06 16:54:43 -0700

    Refactory code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,39 +6,22 @@
 */
 int main(void)
 {
-	int c, d, e, f = 0;
+	int i, j;
 
-	while (f < 10)
+	for (i = 0; i < 100; i++)
 	{
-		e = 0;
-		while (e < 10)
+		for (j = i + 1; j < 100; j++)
 		{
-			d = 0;
-			while (d < 10)
-			{
-				c = 0;
-				while (c < 10)
-				{
-					if (!(f == c && e == d))
-					{
-						putchar('0' + f);
-						putchar('0' + e);
-						putchar(' ');
-						putchar('0' + d);
-						putchar('0' + c);
-						if (!(f + e == 18 && c + d == 17 && d == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					c++;
-				}
-				d++;
-			}
-			e++;
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
-		f++;
 	}
 	putchar('\n');
 	return (0);
