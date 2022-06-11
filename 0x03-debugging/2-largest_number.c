e0ad3f960b70871f006f28f340966acbf034fa35
Sylvain Kadjo <sylvainka12@gmail.com>
2022-06-10 19:08:09 -0700

    Fix condition

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -36,6 +36,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
+	else if (a > b && a >= c)
+	{
+		largest = a;
+	}
+	else if (a >= b && a > c)
+	{
+		largest = a;
+	}
 	else
 	{
 		largest = c;
